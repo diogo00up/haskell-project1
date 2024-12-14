0b146720936c02e0b124649c7edb221449eadcc5
diogo00up <up201905166@up.pt>
2024-12-14 21:07:25 +0000

    play,initial_state,display_game, game_board and auxiliary function to start game

--- src/game.pl
+++ src/game.pl
@@ -49,34 +49,95 @@ choose_difficulty(ch, Difficulty):-
     ;   write('Invalid difficulty. Setting dificulty to "easy".'), nl,
         Difficulty = easy).
 
-choose_difficulty(cc, (Difficulty1 ,Difficulty2)):-
-    write('Set difficulty for Computer 1:'), nl,
+choose_difficulty(cc, Difficulty ):-
     write('Choose difficulty level (easy, medium, hard): '), nl,
-    read(Difficulty1),
-    (   difficulty_level(Difficulty1)
+    read(Difficulty),
+    (   difficulty_level(Difficulty)
     ->  true
     ;   write('Invalid difficulty. Setting dificulty to "easy".'), nl,
-        Difficulty1 = easy),
-    write('Set difficulty for Computer 2:'), nl,
-        write('Choose difficulty level (easy, medium, hard): '), nl,
-    read(Difficulty2),
-    (   difficulty_level(Difficulty2)
-    ->  true
-    ;   write('Invalid difficulty. Setting dificulty to "easy".'), nl,
-        Difficulty2 = easy).
+        Difficulty = easy).
 
 
 difficulty_level(easy).
 difficulty_level(medium).
 difficulty_level(hard).
 
+choose_player(hh, players(Player1,Player2)):-
+    Player1 = player(orange, human),  
+    Player2 = player(blue, human).
+    
+choose_player(hc, players(Player1,Player2)):-
+    Player1 = player(orange, human),  
+    Player2 = player(blue, computer).
+    
+choose_player(ch, players(Player1,Player2)):-
+    Player1 = player(orange, computer),  
+    Player2 = player(blue, human).
+    
+choose_player(cc, players(Player1,Player2)):-
+    Player1 = player(orange, computer),  
+    Player2 = player(blue, computer).
+    
 
 start_game(GameMode, Difficulty) :-
-    % InitialState
-    initialize_game(GameMode, Difficulty).
-
-initialize_game(GameMode, Difficulty) :-
     write('Game initialized with:'),
+    choose_player(GameMode,players(Player1,Player2)),
     nl, write('GameMode: ') ,write(GameMode),write(', '),write('Difficulty: '),  write(Difficulty), nl,
-    write('Game loop begining:').
+    initial_state(game_config(GameMode, Player1, Player2, Difficulty), GameState),
+    game_loop(GameState, Difficulty).
 
+
+play(player(Colour, human),GameState,_):-
+    write('HUMAN PLAYING').
+
+play(player(Colour, computer),GameState,easy):-
+    write('COMPUTER PLAYING'),nl,
+    write('Dificulty: '), write(easy).
+    
+play(player(Colour, computer),GameState,medium):-
+    write('COMPUTER PLAYING'),nl,
+    write('Dificulty: '), write(medium).
+    
+play(player(Colour, computer),GameState,hard):-
+    write('COMPUTER PLAYING'),nl,
+    write('Dificulty: '), write(hard).
+    
+
+game_loop(game_state(Board, CurrentPlayer, NextPlayer, CapturedPieces, RemainingPieces), Difficulty):-
+    display_game(game_state(Board, CurrentPlayer, NextPlayer, CapturedPieces, RemainingPieces)),
+    play(CurrentPlayer,GameState,Difficulty).
+
+
+
+% game_config(GameMode, Player1, Player2, Difficulty)
+% game_state(Board, CurrentPlayer, CapturedPieces, RemainingPieces)
+
+initial_state(game_config(GameMode, Player1, Player2, Difficulty), game_state(Board, CurrentPlayer, NextPlayer, CapturedPieces, RemainingPieces)) :-
+    board(Board),  
+    CurrentPlayer = Player1,
+    NextPlayer = Player2,
+    CapturedPieces = [],
+    RemainingPieces = [].
+    
+
+:- use_module(library(lists)).
+board(Board) :-
+    length(Board, 7),           % Create a list with 7 elements for the rows
+    maplist(create_row, Board). 
+
+create_row(Row) :-
+    length(Row, 7),            
+    maplist(=(empty), Row).    
+
+display_board([]).
+display_board([H|T]):-
+    write(H),nl,
+    display_board(T).
+
+display_game(game_state(Board, CurrentPlayer, NextPlayer, CapturedPieces, RemainingPieces)):-
+    write('CURRENT GAME STATE:'),nl,
+    write('Current player: '),  write(CurrentPlayer), write(', '),
+    write('Next player: '),  write(NextPlayer), write(', '),
+    write('Captured peices: '),  write(CapturedPieces), write(', '),
+    write('Remaining peices: '),  write(RemainingPieces), write(', '),nl,
+    display_board(Board).
